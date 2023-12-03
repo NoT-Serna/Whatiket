1a21723e68acd5ab7a66d48bb5107b9812862911
Juan Serna <jserna7856@gmail.com>
2023-12-02 23:33:17 -0500

    Dashboard

--- tealcrm/tealcrm/settings.py
+++ tealcrm/tealcrm/settings.py
@@ -27,6 +27,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+LOGIN_REDIRECT_URL = '/dashboard/'
+LOGIN_URL = 'login'
+LOGOUT_REDIRECT_URL = 'index'
+
+
 
 # Application definition
 
@@ -39,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'core',
     'userprofile',
+    'dashboard',
 ]
 
 MIDDLEWARE = [
