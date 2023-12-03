7ec1093f99f826bf41c14878d8a23ae1de8463bf
Juan Serna <jserna7856@gmail.com>
2023-12-03 17:27:54 -0500

    Created leads app

--- tealcrm/lead/views.py
+++ tealcrm/lead/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 from .forms import AddLeadForm
 from .models import Lead
@@ -10,6 +11,42 @@ def leads_list(request):
     
     return render(request, 'lead/leads_list.html', {
         'leads': leads
+    })
+
+@login_required
+def leads_detail(request, pk):
+    lead = get_object_or_404(Lead, created_by=request.user, pk=pk)
+
+    return render(request, 'lead/leads_detail.html', {
+        'lead': lead
+    })
+
+@login_required
+def leads_delete(request, pk):
+    lead = get_object_or_404(Lead, created_by=request.user, pk=pk)
+    lead.delete()
+
+    messages.success(request, 'The lead was deleted.')
+
+    return redirect('leads_list')
+
+@login_required
+def leads_edit(request, pk):
+    lead = get_object_or_404(Lead, created_by=request.user, pk=pk)
+
+    if request.method == 'POST':
+        form = AddLeadForm(request.POST, instance=lead)
+
+        if form.is_valid():
+            form.save()
+
+            messages.success(request, 'The lead was modified.')
+            return redirect('leads_list')
+    else:
+        form = AddLeadForm(instance= lead)
+        
+    return render(request, 'lead/leads_edit.html',{
+        'form': form
     })
 
 
@@ -23,7 +60,9 @@ def add_lead(request):
             lead.created_by = request.user 
             lead.save()
 
-            return redirect('dashboard')
+            messages.success(request, 'The lead was added.')
+
+            return redirect('leads_list')
     
     else:
         form = AddLeadForm()
